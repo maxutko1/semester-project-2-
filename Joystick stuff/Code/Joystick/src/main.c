48740e1f59d0bb37e3a8611357703a9811c6bf61
Tolga-Karatoprak <tolga.karatoprak@outlook.com>
2025-05-21 13:36:02 +0200

    Changed code to the joystick code

--- Joystick stuff/Code/Joystick/src/main.c
+++ Joystick stuff/Code/Joystick/src/main.c
@@ -33,27 +33,79 @@ uint16_t adc_read(uint8_t adc_channel){
 uint16_t Joystick_ADC_Values(){ 
   ADMUX = (1<<REFS0);                                       // These bits select the voltage reference for the ADC (AV_CC with external capacitor at AREF pin)
   ADCSRA = (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);      // Enables ADC and sets prescaler to 128
-  adc_result_x = adc_read(0);                               // Reads ADC PIN 0, which is our x axis
-  adc_result_y = adc_read(1);                               // Reads ADC PIN 1, which is our y axis
+  adc_result_x = adc_read(1);                               // Reads ADC PIN 0, which is our x axis
+  adc_result_y = adc_read(2);                               // Reads ADC PIN 1, which is our y axis
 }
 
 void turret_state(void){                                    // Turret state depending on joystick input
+  
   Joystick_ADC_Values();
-    if(adc_result_x>600){                                   // check x-axis adc values
+  
+  int x_direction = 0;                      // Will determine X direction (clockwise or counterclockwise)
+  int x_strenght = 0;                       // Variable that will hold the strenght of the X signal
+  if(adc_result_x > 600){
+    x_direction = 1;                    // Clockwise
+    x_strenght = adc_result_x - 600;    // Strenght of the X signal
+  }
+  if(adc_result_x < 400){
+    x_direction = -1;                   // Counter-clockwise 
+    x_strenght = 400 - adc_result_x;    // Strenght of the X signal
+  }
+
+  int y_direction = 0;                      // Direction (downwards or upwards)
+  int y_strenght = 0;                       // Variable that will hold the strenght of the Y signal
+  if(adc_result_y > 600){
+    y_direction = 1;                    // Downwards
+    y_strenght = adc_result_y - 600;    // Strenght of the Y signal
+  }
+  if(adc_result_y < 400){
+    y_direction = -1;                   // Upwards 
+    y_strenght = 400 - adc_result_y;    // Strenght of the Y signal 
+  }
+
+  if(x_strenght == 0 && y_strenght == 0){
+    return;
+  }
+
+  if(x_strenght > y_strenght){
+    if(x_direction == 1){
       printf("turret is turning clockwise \n");
+    }
+    else if(x_direction == -1){
+      printf("turret is turning counterclock wise \n");
+    }
+  }
+
+  else{
+    if(y_direction == 1){
+      printf("turret is aiming downwards \n");
+    }
+    else if(y_direction == -1){
+      printf("turret is aiming upwards \n");
+    }
+  }
+
+    /*if(adc_result_x>600){                                                    // check x-axis adc values
+      printf("turret is turning clockwise ");
+      printf("%d X, %d Y \n", adc_result_x, adc_result_y);
       //do something ..
     }
-    if(adc_result_x<400){                                   //check x-axis adc values 
-      printf("turret is turning counter-clockwise \n");
+    if(adc_result_x<400 && adc_result_y){                                   //check x-axis adc values 
+      printf("turret is turning counter-clockwise ");
+      printf("%d X, %d Y \n", adc_result_x, adc_result_y);
       // do something ..
     }
     if(adc_result_y<400){                                   //check y-axis adc values 
-      printf("turret is aiming up \n");
+      printf("turret is aiming up ");
+      printf("%d X, %d Y \n", adc_result_x, adc_result_y);
+
       //do something ..
     }
     if(adc_result_y>600){                                   //check y-axis adc values 
-      printf("turret is aiming down \n");
+      printf("turret is aiming down ");
+      printf("%d X, %d Y \n", adc_result_x, adc_result_y);
       //do something ..
-    }
-
+    }*/
 }
+
+
